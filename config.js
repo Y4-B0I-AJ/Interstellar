03bd460efa0c4ca4ac31fb4d7dc216aff9bb15d9
Y4-B0I-AJ <162377787+Y4-B0I-AJ@users.noreply.github.com>
2026-03-17 15:09:43 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const config = true npm start
+const config=true; npm start
   challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
